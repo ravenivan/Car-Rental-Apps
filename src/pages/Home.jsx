d61da807cd0c21d78c1912ecefe4f098e33610a8
Ivan Lam <ravenivan1@gmail.com>
2024-03-06 18:54:16 -0500

    fixed testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Landing from '../components/Landing';
 import Features from '../components/Features';
 import Banner from '../components/Banner';
 import Choose from '../components/Choose';
-import Testimonials from '../components/ui/Testimonial';
+import Testimonials from '../components/Testimonials';
 import Download from '../components/Download';
 
 
